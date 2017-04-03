cae46f774bc2f22350d8fa1d8e8d481788698481
ajet <cbibop.xo@gmail.com>
2017-04-03 22:40:02 +0300

    Готова структура краулера - надо писать логику обхода soup

--- crawler.py
+++ crawler.py
@@ -7,25 +7,93 @@ import os
 import sys
 import json
 import csv
+import uuid.uuid4
 
 import bs4
 import requests
 
-def _get_mode(url):
+
+class  Crawler:
+    """
+    crawler
+    """
+    def __init__(self, path_to_struct=None, target_url=None):
+        """
+        Initialization
+        path_to_struct: path to struct.json
+        target_url: url for request
+        """
+        self._config_path = None
+        self._struct = None
+        self._target_url = None
+
+        if target_url:
+            self._set_target_url(target_url)
+
+        if path_to_struct:
+            self.load_struct(path_to_struct)
+
+    def load_struct(self, path_to_struct):
+        """
+        load struct from file
+        path_to_struct: path to struct.json
+        """
+        if os.path.exists(path_to_struct) is False:
+            msg = 'Check path {}'.format(path_to_struct)
+            raise FileNotFoundError(msg)
+
+        self._config_path = path_to_struct
+        self._struct = json.loaddata(open(path_to_struct, 'rt').read())
+
+    def _set_target_url(self, target_url):
+        """
+        set target url
+        target_url: target url
+        """
+        if  target_url.split('://')[0] != 'http':
+            msg = 'Check target url {}'.format(target_url)
+            raise SyntaxError(msg)
+
+        self._target_url = target_url
+
+    def get_one_page(self):
+        """
+        get one page generator
+        """
+        one_page_data = []
+        resp = requests.get(self.target_url)
+        if resp.status_code != 200:
+            msg = 'url - is not available, status code {}'.format(\
+                resp.status_code)
+            raise IOError(msg)
+
+        soup = bs4(resp.text)
+            
+        yield one_page_data
+
+    def get_all_data(self):
+        """
+        Get all data from target url by installed struct
+        """
+        all_data = [] 
+        for one_page_data in get_one_page():
+            all_data = all_data + one_page_data
+
+        return all_data
+
+
+    def __repr__(self):
+        """
+        print instance
+        """
+        print('Crowler, config: {}, url in_work: {}'.format(\
+            self._config_path, self._target_url))
+
+def _get_mode(struct_path, url):
     """
     Get data from url
     """
-    class  Crawler:
-        def __init__(self, path_to_struct):
-            """
-            Initialization
-            path_to_struct: path to struct.json
-            """
-            if os.path.exists(arg) is False:
-                msg = 'Check path {}'.format(arg)
-                raise FileNotFoundError(msg)
-            in_put = open(arg, 'rt')
-            self.struct = json.loaddata()
+    crawler = Crawler()
 
 def _run_unittests():
     """
@@ -69,7 +137,7 @@ def _run_unittests():
                                 "class": "Target_folder_title",
                                 "target":null,
                                 "next" :{
-                                    "tag": "h3",
+                                    "tag": "a",
                                     "class": "Target_folder_title_description",
                                     "target":"title",
                                     "next" :null
