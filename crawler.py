2fbd8e855d84db69130e4db67be9929d5ec67fd1
ajet <cbibop.xo@gmail.com>
2017-04-08 17:28:02 +0300

    тесты проходит - готов краулер

--- crawler.py
+++ crawler.py
@@ -7,11 +7,29 @@ import os
 import sys
 import json
 import csv
-import uuid
 
 from bs4 import BeautifulSoup
 import requests
 
+def _get_data_by_struct(struct, soup, add_text=False):
+    """
+    """
+    out = []
+    for inst in soup.body.findAll(\
+        struct.get('tag'), class_=struct.get('class')):
+        target = struct.get('target')
+
+        if add_text:
+            data = [inst.get(target), getattr(inst,'text')]
+        else:
+            data = inst.get(target)
+        out.append(data)
+
+    if len(out) == 0:
+        msg = 'cant find struct {} in html'.format(struct)
+        raise SyntaxError(msg)
+
+    return out
 
 class  Crawler:
     """
@@ -45,6 +63,11 @@ class  Crawler:
         self._config_path = path_to_struct
         self._struct = json.loads(open(path_to_struct, 'rt').read())
 
+    def set_struct(self, struct_dict):
+        """
+        """
+        self._struct = struct_dict
+
     def _set_target_url(self, target_url):
         """
         set target url
@@ -56,53 +79,25 @@ class  Crawler:
 
         self._target_url = target_url
 
-    def _struct_wolker(self, struct_key, soup):
-        """
-        Parse text
-        struct_key: ключ искомой структуры
-        """
-        out = []
-        node = self._struct.get(struct_key)
-        soup = soup
-
-        for inst in soup.body.findAll(node.get('tag')):
-            cls_attr = inst.get('class')
-            if len(inst.get('class')) !=0:
-                if inst.pop() == node.get('cls_attr'):
-                    out.append(node.get('target'))                    
-
-        return out
 
     def get_one_page_data(self, url):
         """
         get one page generator
         """
-        one_page_data = []
-        next_url = None
         resp = requests.get(url)
         if resp.status_code != 200:
             msg = 'url - is not available, status code {}'.format(\
                 resp.status_code)
             raise IOError(msg)
 
-        node = self._struct.get('items')
         soup = BeautifulSoup(resp.text, 'html.parser')
+        one_page_data = _get_data_by_struct(self._struct.get('items'), soup)
 
-        for inst in soup.body.findAll(node.get('tag')):
-            cls_attr = inst.get('class')
+        next_url = _get_data_by_struct(self._struct.get('next_page'),\
+             soup, add_text=True)
+        next_url = next_url.pop()
 
-            if len(cls_attr) !=0:
-                cls_attr = cls_attr.pop()
-
-
-                if cls_attr == node.get('cls_attr'):
-                    if node.get('text') is None:
-                        one_page_data.append(node.get('target'))                    
-                    if node.get('text') == inst.get('text'):
-                        next_url = inst.get(node.get('target'))
-
-
-        yield one_page_data, next_url
+        return one_page_data, next_url
 
 
     def get_all_pages_data(self):
@@ -110,11 +105,17 @@ class  Crawler:
         Get all data from target url by installed struct
         """
         all_data = []
-        soup = BeautifulSoup(resp.text, 'html.parser')
-
-        urls = [self.target_url]
-        for one_page_data in self.get_one_page():
+        page_marker = None
+        next_url = [self._target_url]
+        while next_url is not None:
+            one_page_data, next_url = self.get_one_page_data(next_url[0])
             all_data = all_data + one_page_data
+
+            if not page_marker:
+                page_marker = next_url[1]
+
+            if  page_marker != next_url[1]:
+                next_url = None
 
         return all_data
 
@@ -130,7 +131,9 @@ def _get_mode(struct_path, url):
     """
     Get data from url
     """
-    crawler = Crawler()
+    crawler = Crawler(struct_path, url)
+    data = crawler.get_all_pages_data()
+    print(crawler, len(data))
 
 def _run_unittests():
     """
@@ -143,7 +146,6 @@ def _run_unittests():
         """
         Test for crawler
         """
-        @responses.activate
         def setUp(self):
             """
             setup test data
@@ -156,41 +158,25 @@ def _run_unittests():
                 'next_page':struct_node('a', 'pagination-nav js-pagination-next',\
                     'herf', 'Следующая страница')})
 
-#            self._struct_json = '{' + '"item_folder":{0}, "item":{1}, "next_page":{2}'.format(\
-#                    struct_node("body", "null", "null", "null",\
-#                        struct_node("div", "first_level", "null", "null",\
-#                                struct_node("div", "second_level", "text", "null", "null")\
-#                            )\
-#                        ),\
-#                    struct_node("div", "item", "null", "null",\
-#                        struct_node("h3", "item_title", "null", "null",\
-#                            struct_node("a", "item_title_desription", "title", "null", "null")\
-#                        )\
-#                    ),\
-#                    struct_node("div", "pagination js-pages", "null", "null",\
-#                        struct_node("div", "pagination-nav clearfix", "null", "null",\
-#                            struct_node("a", "pagination-nav js-pagination-next",\
-#                                "herf", "Следующая страница", "null")\
-#                        )\
-#                    )\
-#            ) + '}'
-
             self._first_url = 'http://test.com/catalog/1'
             self._second_url = 'http://test.com/catalog/2'
 
             head = '<html><head><title>Page title</title></head>'
             html_node = lambda tag, attr, attr_value, text: '<{0} {1}="{2}">{3}</{0}>'.\
                 format(tag, attr, attr_value, text)
-            html_a_node = lambda tag, cls_value, herf_val, text:\
+
+            html_a_item_node = lambda tag, cls_value, title_val, text:\
+                '<{0} class="{1}" title={2}>{3}</{0}>'.format(tag, cls_value, title_val, text)
+            html_a_link_node = lambda tag, cls_value, herf_val, text:\
                 '<{0} class="{1}" herf={2}>{3}</{0}>'.format(tag, cls_value, herf_val, text)
 
             first_item = html_node('div', 'class', 'item',\
                  html_node('h3', 'class', 'item_title',\
-                    html_node('a', 'class', 'item_title_desription', '1_p FIRST ITEM')))
+                    html_a_item_node('a', 'item_title_desription', '"1_p FIRST ITEM"', '1_p FIRST ITEM')))
 
             second_item = html_node('div', 'class', 'item',\
                  html_node('h3', 'class', 'item_title',\
-                    html_node('a', 'class', 'item_title_desription', '1_p SECOND ITEM')))
+                    html_a_item_node('a', 'item_title_desription', '"1_p SECOND ITEM"', '1_p SECOND ITEM')))
 
             body = html_node('body', 'class', ' ',\
                 html_node('div', 'class', 'first_level',\
@@ -199,20 +185,19 @@ def _run_unittests():
                 ) +\
                 html_node('div', 'class', 'pagination js-pages',\
                     html_node('div', 'class', 'pagination-nav clearfix',\
-                        html_a_node('a', 'pagination-nav js-pagination-next', self._second_url,\
+                        html_a_link_node('a', 'pagination-nav js-pagination-next', self._second_url,\
                          'Следующая страница →'))\
                 )\
             )
-
             self._first_html = head + body
 
             first_item = html_node('div', 'class', 'item',\
                  html_node('h3', 'class', 'item_title',\
-                    html_node('a', 'class', 'item_title_desription', '2_p FIRST ITEM')))
+                    html_a_item_node('a', 'item_title_desription', '"2_p FIRST ITEM"', '2_p FIRST ITEM')))
 
             second_item = html_node('div', 'class', 'item',\
                  html_node('h3', 'class', 'item_title',\
-                    html_node('a', 'class', 'item_title_desription', '2_p SECOND ITEM')))
+                    html_a_item_node('a', 'item_title_desription', '"2_p SECOND ITEM"', '2_p SECOND ITEM')))
 
             body = html_node('body', 'class', ' ',\
                 html_node('div', 'class', 'first_level',\
@@ -221,28 +206,38 @@ def _run_unittests():
                 ) +\
                 html_node('div', 'class', 'pagination js-pages',\
                     html_node('div', 'class', 'pagination-nav clearfix',\
-                        html_a_node('a', 'pagination-nav js-pagination-next', self._first_url,\
+                        html_a_link_node('a', 'pagination-nav js-pagination-next', self._first_url,\
                          '← Предыдущая'))\
                 )\
             )
             self._second_html = head + body
 
-            responses.add(responses.GET, self._first_url, self._first_html, status=200)
-            responses.add(responses.GET, self._second_url, self._second_html, status=200)
 
+        @responses.activate
         def test_get_html_from_url(self):
             """
             test get html
             """
-            crawler = Crawler(target_url=self._first_url)
-            crawler._struct = json.loads(self._struct_json)
-#            crawler.get_one_page()
+            responses.add(responses.GET, self._first_url, self._first_html, status=200)
 
+            crawler = Crawler(target_url=self._first_url)
+            crawler.set_struct(json.loads(self._struct_json))
+            data, url = crawler.get_one_page_data(crawler._target_url)
+            self.assertListEqual(data, ['1_p FIRST ITEM', '1_p SECOND ITEM'])
+            self.assertEqual(url[0], self._second_url)
+
+        @responses.activate
         def test_get_next_page(self):
             """
             test get html
             """
-            pass
+            responses.add(responses.GET, self._first_url, self._first_html, status=200)
+            responses.add(responses.GET, self._second_url, self._second_html, status=200)
+
+            crawler = Crawler(target_url=self._first_url)
+            crawler.set_struct(json.loads(self._struct_json))
+            data = crawler.get_all_pages_data()
+            self.assertListEqual(data, ['1_p FIRST ITEM', '1_p SECOND ITEM', '2_p FIRST ITEM', '2_p SECOND ITEM'])
 
         def test_save_data(self):
             """
